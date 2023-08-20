878a2a5abc5cc029b073327367e5702c67b3780d
UnnatiVe <unnativerma20022003@gmail.com>
2023-08-20 15:40:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import First from './First';
+
+function App() {
+  return (
+  <>  
+  <First />
+   <div className='box1'>
+      <h1>Unnati Verma</h1>
+      <div style={{color:"black"}}>Chandigarh</div>
+    </div>
+   
+  </>
+  );
+}
+
+export default App;
---
+++ src/First.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function First() {
+  return (
+  <>  
+    <div className='box2'>
+      <h1>Utsav Verma</h1>
+    </div>
+  </>
+  );
+}
+
+export default First;
